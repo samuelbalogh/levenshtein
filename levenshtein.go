ef82b4f7c64b7a99fae478717b16a2b8dcadba42
Samuel Balogh <samuel@ometria.com>
2019-07-29 13:48:52 +0100

    go fmt

--- levenshtein.go
+++ levenshtein.go
@@ -5,19 +5,19 @@ import (
 )
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
-func Min(array [3]int) (int) {
-    var min int = array[0]
-    for _, value := range array {
-        if min > value {
-            min = value
-        }
-    }
-    return min
+func Min(array [3]int) int {
+	var min int = array[0]
+	for _, value := range array {
+		if min > value {
+			min = value
+		}
+	}
+	return min
 }
 
 func printLevMatrix(source string, target string, a [][]int) {
@@ -33,13 +33,12 @@ func printLevMatrix(source string, target string, a [][]int) {
 	}
 }
 
-
-func GetLevMatrix(source, target string) ([][]int){
+func GetLevMatrix(source, target string) [][]int {
 	// Levenshtein distance
 	// https://en.wikipedia.org/wiki/Levenshtein_distance#Iterative_with_full_matrix
-	a := make([][]int, len(source) + 1)
+	a := make([][]int, len(source)+1)
 	for i := range a {
-    		a[i] = make([]int, len(target) + 1) 
+		a[i] = make([]int, len(target)+1)
 	}
 
 	for i := range a {
@@ -49,37 +48,36 @@ func GetLevMatrix(source, target string) ([][]int){
 	for j := range a[0] {
 		a[0][j] = j
 	}
-        substitutionCost := 0
+	substitutionCost := 0
 
-        for j := 1; j <= len(target); j++ {
-        	for i := 1; i <= len(source); i++ {
-          		if source[i - 1] == target[j -1] {
-            			substitutionCost = 0
+	for j := 1; j <= len(target); j++ {
+		for i := 1; i <= len(source); i++ {
+			if source[i-1] == target[j-1] {
+				substitutionCost = 0
 			} else {
-            			substitutionCost = 1
+				substitutionCost = 1
 			}
-          		
+
 			changes := [...]int{
-			     a[i-1][j] + 1,  			// deletion
-                             a[i][j-1] + 1,                     // insertion
-                             a[i-1][j-1] + substitutionCost}    // substitution
+				a[i-1][j] + 1,                  // deletion
+				a[i][j-1] + 1,                  // insertion
+				a[i-1][j-1] + substitutionCost} // substitution
 
 			a[i][j] = Min(changes)
-		
+
 		}
 	}
 
-	return a 
+	return a
 
 }
 
-func GetDistance(levMatrix [][]int, source string, target string) (int) {
+func GetDistance(levMatrix [][]int, source string, target string) int {
 	distance := levMatrix[len(source)][len(target)]
 	return distance
 }
 
-
-func CalculateDistance(source string, target string) (int) {
+func CalculateDistance(source string, target string) int {
 	mtx := GetLevMatrix(source, target)
 	distance := GetDistance(mtx, source, target)
 	return distance
